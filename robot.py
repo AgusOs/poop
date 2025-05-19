71eab8df679829be1279b641ce382015a26c02e6
AgusOs <osorioagus96@gmail.com>
2025-05-19 08:34:11 -0300

    some changes

--- robot.py
+++ robot.py
@@ -12,8 +12,16 @@ class Robot:
     def saludar(self):
         print(f"¡Hola! Soy {self.nombre} y soy un robot en una muestra de Robots.")
 
+    def hacerTruco(self):
+        if self.tipo == "Humanoide":
+            print(f"Soy {self.nombre} y estoy haciendo el paso del robot")
+        else:
+            print(f"Soy {self.tipo} de tipo {self.tipo}")
+
 #Instancia
 robotin = Robot("Robotín", "Humanoide")
+
+robotin.hacerTruco()
 
 print(robotin.nombre)
 print(robotin.tipo)
